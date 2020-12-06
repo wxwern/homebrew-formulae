8cd92d4303aa68f9a764faf75662a43792ff11c0
Wern Jie Lim <lwjdev@gmail.com>
2020-12-07 02:38:24 +0800

    added p.diary v2.6.2

--- Casks/personal-diary-beta.rb
+++ Casks/personal-diary-beta.rb
@@ -1,6 +1,6 @@
 cask 'personal-diary-beta' do
-  version '2.6.1'
-  sha256 'fc64cd8c3a49e4b3c65c810084df81329a2c4acaa236b4a9ba20916601fa633b'
+  version '2.6.2'
+  sha256 '6b6a47d3a3e58c26971863baf396a1c2c15dd9730671f47fbe45ffba16693962'
 
   url "https://github.com/wernjie/personal-diary/releases/download/v#{version}/Personal-Diary-macOS-v#{version}.zip"
   name 'Personal Diary'
